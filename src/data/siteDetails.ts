7aecb4e89c1ad5be65c1a5e832e7962f84c197bd
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-07-13 00:45:59 +0300

    update

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -2,7 +2,7 @@ export const siteDetails = {
     siteName: 'Finwise',
     siteUrl: 'https://finwise-omega.vercel.app/',
     metadata: {
-        title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
+        title: 'Finwise - Next.js and Tailwind CSS Landing Page',
         description: 'Finwise empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
     },
     language: 'en-us',
